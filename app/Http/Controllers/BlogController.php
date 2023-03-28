d29702d65d1bce8dc22596f34695c09db8e57f98
Nithin105 <123984431+Nithin105@users.noreply.github.com>
2023-03-28 14:00:47 +0100

    changes

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -1,1 +1,29 @@
- 
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class BlogController extends Controller
+{
+    public function index()
+    {
+        $blogs = Blog::all();
+        return view('admin', compact('blogs'));
+    }
+    public function store(Request $request)
+    {        $user = Auth::user();
+
+        $blog = new Blog;
+        $blog->title = $request->input('title');
+        $blog->abstract = $request->input('abstract');
+        $blog->Content = $request->input('Content');
+        $blog->product = $request->input('product');
+        $blog->user_id =  $user->id;
+
+        $blog->save();
+
+        return redirect()->route('admin');
+    }
+}
